f6a78db38d7d76e8f17cd37b27e744b8c5671fd2
ardentsolitude <113372727+ardentsolitude@users.noreply.github.com>
2024-04-30 10:40:22 +0100

    Refining and commenting

--- CMP1903_A1_2324/SevensOut.cs
+++ CMP1903_A1_2324/SevensOut.cs
@@ -27,6 +27,10 @@ namespace CMP1903_A1_2324 {
 
 		}
 
+		/// <summary>
+		/// Play a game of sevens out
+		/// </summary>
+		/// <param name="playerCount"></param>
 		public override void playGame(int playerCount) {
 			Die die1 = new Die();
 			Die die2 = new Die();
@@ -93,17 +97,17 @@ namespace CMP1903_A1_2324 {
 						Console.ReadLine();
 						Console.WriteLine($"Player {playerTurn + 1} Rolled {die1roll} and {die2roll}, giving a total of {die1roll + die2roll}.\n");
 					}
-					if (die1roll == die2roll) {
+					if (die1roll == die2roll) { //if double
 						Console.WriteLine("Rolled A Double! \nDouble Points!");
-						p2Score = p2Score + (die1roll + die2roll) * 2;
+						p2Score = p2Score + (die1roll + die2roll) * 2; //add double the points
 					}
 					else {
-						if (die1roll + die1roll != 7) {
-							p2Score = p2Score + die1roll + die2roll;
+						if (die1roll + die1roll != 7) { //if not double
+							p2Score = p2Score + die1roll + die2roll; //add sum
 						}
 					}
 
-					if (die1roll + die2roll == 7) {
+					if (die1roll + die2roll == 7) { //I'm assuming that you score 0 points on a 7
 						Console.WriteLine("You Got A 7! \nYou're Out!\n");
 						p2Out = true;
 					}
@@ -158,14 +162,26 @@ namespace CMP1903_A1_2324 {
 			Stats.updateStats(winningScore, "Sevens", winner);
 			returnToMenu();
 		}
-
-		public void writeLog(int die1, int die2, TextWriter logWriter, int playerTurn) {
+		/// <summary>
+		/// Write a log of when a 7 is rolled to a text file
+		/// </summary>
+		/// <param name="die1"></param>
+		/// <param name="die2"></param>
+		/// <param name="logWriter"></param>
+		/// <param name="playerTurn"></param>
+		public void writeLog(int die1, int die2, TextWriter logWriter, int playerTurn) { //Write info to log
 			logWriter.WriteLine($"\nDate: {DateTime.Today.ToString("d")}\nTime: {DateTime.Now.ToString("HH:mm:ss")}\nPlayer {playerTurn + 1} Rolled 7 ({die1}, {die2})\n"); //Write to log file
 		}
 
 
 
 		//Testing Version - Records Logs
+		/// <summary>
+		/// Play a game of sevens out - records logs
+		/// </summary>
+		/// <param name="playerCount"></param>
+		/// <param name="testingMode"></param>
+		/// <param name="logSaveLocation"></param>
 		public override void playGame(int playerCount, bool testingMode, string logSaveLocation) {
 			Die die1 = new Die();
 			Die die2 = new Die();
@@ -190,8 +206,11 @@ namespace CMP1903_A1_2324 {
 					//This was originally a separate function, but I merged it int0 this one to make it easier to work with
 					int die1roll = die1.rollDie();
 					Thread.Sleep(500); //Prevents duplicate numbers, also slows pace of game to for readability reasons
+					//because in testing I found that without a delay, the game would progress faster than the player could keep up
 					int die2roll = die2.rollDie();
 
+
+					//Turn Start
 					Console.WriteLine($"\nPlayer {playerTurn + 1}'s Turn!\nPress Enter To Begin.");
 					Console.ReadLine();
 					Console.WriteLine($"Player {playerTurn + 1} Rolled {die1roll} and {die2roll}, giving a total of {die1roll + die2roll}.\n");
@@ -212,26 +231,14 @@ namespace CMP1903_A1_2324 {
 
 					}
 
-					if (testingMode == true) {
-						if (p1Out == true) {
-							//string logSaveLocation = System.IO.Directory.GetCurrentDirectory();
-							//Console.WriteLine(logSaveLocation);
-							using (StreamWriter logWriter = File.AppendText(logSaveLocation + "\\logFile.txt")) {
-								writeLog(die1roll, die2roll, logWriter, playerTurn);
-							}
-						}
-						Debug.Assert(p1Out == false, $"Player 1 Rolled A Seven. Log Saved To {logSaveLocation}. Check Log File For More Information.");
-
-					}
-
 					//Log Recording
 					if (testingMode == true) {
-						if (p2Out == true) {
+						if (p1Out == true) {
 							//string logSaveLocation = System.IO.Directory.GetCurrentDirectory();
 							try {
 								//Console.WriteLine(logSaveLocation);
 								using (StreamWriter logWriter = File.AppendText(logSaveLocation + "\\logFile.txt")) {
-									writeLog(die1roll, die2roll, logWriter, playerTurn);
+									writeLog(die1roll, die2roll, logWriter, playerTurn); 
 								}
 							}
 							catch (DirectoryNotFoundException error) {
@@ -239,6 +246,7 @@ namespace CMP1903_A1_2324 {
 							}
 
 						}
+						//Debug.assert is after log saving because if it is before, it can prevent the log from being written
 						Debug.Assert(p2Out == false, $"Player 2 Rolled A Seven. Log Saved To {logSaveLocation}. Check Log File For More Information.");
 
 					}
@@ -301,7 +309,7 @@ namespace CMP1903_A1_2324 {
 
 			//I am not proud of the above code
 
-
+			//End of game and calculating winner
 			Console.WriteLine("\nGame Over!\n");
 			int winner = 0;
 			int winningScore = 0;
@@ -336,8 +344,8 @@ namespace CMP1903_A1_2324 {
 			//Statistics handling
 
 			//Exit
-			Stats.updateStats(winningScore, "Sevens", winner);
-			returnToMenu();
+			Stats.updateStats(winningScore, "Sevens", winner); //Update statistics
+			returnToMenu(); //Go back to main menu
 
 		}
 	}
